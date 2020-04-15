5de6516a127d8dee70192362076235d3b58751ad
Sinan BOZKUŞ <sinan.bozkus@gmail.com>
2020-04-16 01:56:49 +0300

    initialt

---
+++ BackupAssistant/BackupAssistant/BackupManager.cs
@@ -0,0 +1,80 @@
+﻿using BackupAssistant.Contracts.Types;
+using BackupAssistant.Providers.FtpServer;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackupAssistant
+{
+    public class BackupManager
+    {
+        private readonly AppSettings _appSettings;
+        private readonly IFtpServerProvider _ftpServerProvider;
+
+        public BackupManager(IOptions<AppSettings> appSettings,
+            IFtpServerProvider ftpServerProvider)
+        {
+            _appSettings = appSettings.Value;
+            _ftpServerProvider = ftpServerProvider;
+        }
+
+        public async Task Run()
+        {
+            var backupPaths = _appSettings.BackupPaths;
+
+            foreach (var backupPath in backupPaths)
+            {
+                var localFileList = Directory.GetFiles(backupPath.LocalPath)
+                    .Where(x => _appSettings.BackupFileExtensions.Contains(Path.GetExtension(x).ToLower()))
+                    .ToList();
+
+                List<string> backupFileList;
+
+                if (_appSettings.ZipFilesBeforeSend)
+                {
+                    backupFileList = new List<string>();
+
+                    foreach (var item in localFileList)
+                    {
+                        var zipFilePath = Path.ChangeExtension(item, ".zip");
+
+                        using (var archive = ZipFile.Open(zipFilePath, ZipArchiveMode.Create))
+                        {
+                            archive.CreateEntryFromFile(item, Path.GetFileName(item));
+                        }
+
+                        backupFileList.Add(zipFilePath);
+                    }
+                }
+                else
+                {
+                    backupFileList = localFileList;
+                }
+
+                if (_appSettings.Providers.FtpServer.Enabled)
+                {
+                    await _ftpServerProvider.SendFilesToServer(backupPath.RemotePath, backupFileList);
+                }
+
+                if (_appSettings.ZipFilesBeforeSend && _appSettings.DeleteZipFilesAfterSend)
+                {
+                    foreach (var backupFile in backupFileList)
+                    {
+                        File.Delete(backupFile);
+                    }
+                }
+
+                if (_appSettings.DeleteFilesAfterSend)
+                {
+                    foreach (var localFile in localFileList)
+                    {
+                        File.Delete(localFile);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ BackupAssistant/BackupAssistant/Program.cs
@@ -0,0 +1,33 @@
+﻿using BackupAssistant.Contracts.Types;
+using BackupAssistant.Providers.FtpServer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+
+namespace BackupAssistant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // AppSettings
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            var configuration = builder.Build();
+
+            // Dependency Injection
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddOptions();
+            serviceCollection.Configure<AppSettings>(configuration.GetSection(nameof(AppSettings)));
+            serviceCollection.AddScoped<IFtpServerProvider, FtpServerManager>();
+            serviceCollection.AddScoped<BackupManager>();
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var backupManager = serviceProvider.GetService<BackupManager>();
+            backupManager.Run().Wait();
+        }
+    }
+}
